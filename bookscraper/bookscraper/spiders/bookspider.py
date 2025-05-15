f377f0df52a957e64c883054d043d71dbbb97744
soethiha <usersoe123@gmail.com>
2025-05-15 17:57:13 +0630

    final

---
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -0,0 +1,81 @@
+from urllib.parse import urlencode
+import scrapy
+from bookscraper.items import BookItem
+import random
+
+API_KEY = "ff97c408-d847-495f-9d82-ec9f04f0434e"
+
+def get_proxy_url(url):
+    payload = { 'api_key': API_KEY, 'url': url }
+    proxy_url = 'https://proxy.scrapeops.io/v1/?' + urlencode(payload)
+    return proxy_url
+
+class BookspiderSpider(scrapy.Spider):
+    name = "bookspider"
+    allowed_domains = ["books.toscrape.com", "proxy.scrapeops.io"]
+    start_urls = ["https://books.toscrape.com"]
+
+    custom_settings = {
+        'FEEDS': {
+            'bookdata.json': {'format': 'json', 'overwrite': True}
+        }
+    }
+
+    user_agent_lists = [
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:127.0) Gecko/20100101 Firefox/127.0',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:127.0) Gecko/20100101 Firefox/127.0',
+        'Mozilla/5.0 (X11; Linux i686; rv:127.0) Gecko/20100101 Firefox/127.0',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4 Safari/605.1.15',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.2535.79',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 OPR/98.0.4759.103 Safari/537.36',
+        'Mozilla/5.0 (Linux; Android 14; Pixel 8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Mobile Safari/537.36',
+        'Mozilla/5.0 (Linux; Android 14; Samsung SM-S928B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Mobile Safari/537.36',
+        'Mozilla/5.0 (Android 14; Mobile; rv:127.0) Gecko/20100101 Firefox/127.0',
+        'Mozilla/5.0 (iPhone; CPU iPhone OS 17_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4 Mobile/15E148 Safari/604.1',
+        'Mozilla/5.0 (Linux; Android 14; Pixel 8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Mobile Safari/537.36 EdgA/125.0.2535.79',
+        'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
+        'Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)',
+    ]
+
+    def parse(self, response):
+        books = response.css('article.product_pod')
+
+        for book in books:
+            relative_url = book.css('h3 a ::attr(href)').get()
+            if 'catalogue/' in relative_url:
+                book_url = 'https://books.toscrape.com/' + relative_url
+            else:
+                book_url = 'https://books.toscrape.com/catalogue/' + relative_url
+            yield response.follow(book_url, callback= self.parse_book_page)
+            
+            
+        next_page = response.css('.next a ::attr(href)').get()
+        if next_page != None:
+            if 'catalogue/' in next_page:
+                next_page_url = 'https://books.toscrape.com/' + next_page
+            else:
+                next_page_url = 'https://books.toscrape.com/catalogue/' + next_page
+            yield response.follow(next_page_url, callback= self.parse)
+
+    def parse_book_page(self, response):
+        table_rows = response.css("table tr")
+        book_item = BookItem()
+        
+        book_item['url'] = response.url,
+        book_item['title'] = response.css('.product_main h1::text').get(),
+        book_item['upc'] = table_rows[0].css("td ::text").get(),
+        book_item['product_type'] = table_rows[1].css("td ::text").get(),
+        book_item['price_excl_tax'] = table_rows[2].css("td ::text").get(),
+        book_item['price_incl_tax'] = table_rows[3].css("td ::text").get(),
+        book_item['tax'] = table_rows[4].css("td ::text").get(),
+        book_item['availability'] = table_rows[5].css("td ::text").get(),
+        book_item['num_reviews'] = table_rows[6].css("td ::text").get(),
+        book_item['stars'] = response.css('p.star-rating').attrib['class'],
+        book_item['category'] = response.xpath("//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()").get(),
+        book_item['description'] = response.xpath("//div[@id='product_description']/following-sibling::p/text()").get(),
+        book_item['price'] = response.css("p.price_color ::text").get(),
+        
+        yield book_item
